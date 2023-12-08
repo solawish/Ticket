78432e26750edc3e2a31576c13b6a6291cef50ec
DESKTOP-E006J8C\User <henrywoody@gmail.com>
2023-12-09 00:42:58 +0900

    feature: 密碼使用MD5加密

--- Ticket.Application/Queries/TicketPlus/GetAccessToken/GetAccessTokenQuery.cs
+++ Ticket.Application/Queries/TicketPlus/GetAccessToken/GetAccessTokenQuery.cs
@@ -8,8 +8,5 @@ public class GetAccessTokenQuery : IRequest<GetAccessTokenDto>
 
     public string CountryCode { get; set; }
 
-    /// <summary>
-    /// 加密過的密碼
-    /// </summary>
     public string Password { get; set; }
 }
