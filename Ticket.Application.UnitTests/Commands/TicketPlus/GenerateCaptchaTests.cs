58a9971190ca1765282ed366b95256fc2e4e25c9
DESKTOP-E006J8C\User <henrywoody@gmail.com>
2023-12-12 01:04:55 +0900

    test: GenerateCaptcha單元測試, 修正檢察post content(小駝峰)

--- Ticket.Application.UnitTests/Commands/TicketPlus/GenerateCaptchaTests.cs
+++ Ticket.Application.UnitTests/Commands/TicketPlus/GenerateCaptchaTests.cs
@@ -4,42 +4,36 @@ using Microsoft.Extensions.Options;
 using Moq;
 using RichardSzalay.MockHttp;
 using Shouldly;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
-using Ticket.Application.Helpers;
+using Ticket.Application.Commands.TicketPlus.GenerateCaptcha;
 using Ticket.Application.Options;
-using Ticket.Application.Queries.TicketPlus.GetAccessToken;
 using Ticket.Application.UnitTests.AutoFixtureSettings;
 using Xunit;
 
-namespace Ticket.Application.UnitTests.Queries.TicketPlus;
+namespace Ticket.Application.UnitTests.Commands.TicketPlus;
 
-[ExcludeFromCodeCoverage]
-public class GetAccessTokenHandlerTests
+public class GenerateCaptchaTests
 {
     [Theory]
     [AutoTestingData]
-    public async Task Handle_GetAccessTokenHandler_GiveValidRequest_ShouldReturnAccessToken(
+    public async Task Handle_GenerateCaptcha_GiveValidRequest_ShouldReturnCaptcha(
         IFixture fixture,
         [Frozen] Mock<IHttpClientFactory> httpClientFactory,
-        [Frozen] Mock<IMD5Helper> md5Helper,
         IOptions<TicketPlusOptions> options,
-        GetAccessTokenHandler sut
+        GenerateCaptchaCommandHandler sut
         )
     {
         // Arrange
-        var request = fixture.Create<GetAccessTokenQuery>();
+        var request = fixture.Create<GenerateCaptchaCommand>();
         var response = fixture
-            .Build<GetAccessTokenDto>()
+            .Build<GenerateCaptchaDto>()
             .Create();
-
-        var encryptedPassword = fixture.Create<string>();
-        md5Helper.Setup(x => x.GetMd5Hash(It.IsAny<string>())).Returns(encryptedPassword);
 
         var mockHttp = new MockHttpMessageHandler();
         var mockRequest = mockHttp
-            .When(HttpMethod.Post, options.Value.LoginUrl)
-            .WithJsonContent<GetAccessTokenQuery>(x => x.Password.Equals(encryptedPassword), new JsonSerializerOptions(JsonSerializerDefaults.Web))
+            .When(HttpMethod.Post, options.Value.GenerateCaptchaUrl)
+            .WithHeaders("Authorization", $"Bearer {request.Token}")
+            .WithJsonContent(request, new JsonSerializerOptions(JsonSerializerDefaults.Web))
             .Respond("application/json", JsonSerializer.Serialize(response));
 
         var httpClient = new HttpClient(mockHttp);
@@ -50,7 +44,6 @@ public class GetAccessTokenHandlerTests
 
         // Assert
         result.ShouldNotBeNull();
-        result.UserInfo.ShouldNotBeNull();
         mockHttp.GetMatchCount(mockRequest).ShouldBe(1);
     }
 }
